7beeab1025f21181baa17c06ba020bf3fdfccad5
Aventix <melvinschneider02@gmail.com>
2026-04-26 12:23:21 +0200

    feat(api): add health endpoint

---
+++ services/api/src/main/kotlin/dev/aventix/stationmanager/api/common/security/SecurityConfiguration.kt
@@ -0,0 +1,21 @@
+package dev.aventix.stationmanager.api.common.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+class SecurityConfiguration {
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        return http
+            .csrf { it.disable() }
+            .authorizeHttpRequests {
+                it
+                    .requestMatchers("/api/health").permitAll()
+                    .anyRequest().authenticated()
+            }
+            .build()
+    }
+}
